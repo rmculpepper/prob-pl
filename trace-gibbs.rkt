7f643520cc4ed7c1e1c28830f34da93ad3880647
Ryan Culpepper <ryanc@racket-lang.org>
2015-05-03 23:05:49 -0400

    rename files for additional trace-based techniques
    original commit: 54a671841a3a48b226e4421593a3862a094a9c02

--- trace-gibbs.rkt
+++ trace-gibbs.rkt
@@ -6,43 +6,6 @@
          "trace-mh.rkt")
 (provide (all-defined-out))
 
-;; slice-trace : Trace TraceVar -> Trace
-(define (slice-trace trace tvar)
-  (slice-trace* trace (hash tvar #t)))
-
-;; slice-trace* : Trace Hash[Tvar => #t] -> Trace
-(define (slice-trace* trace rel)
-  (match trace
-    [(cons (and (ts:primop var primop args) ts) trace-rest)
-     (cond [(any-relevant? rel args)
-            (let ([rel* (hash-set rel var #t)])
-              (cons ts (slice-trace* trace-rest rel*)))]
-           [else (slice-trace* trace-rest rel)])]
-    [(cons (and (ts:sample var dist value) ts) trace-rest)
-     (cond [(relevant? rel value)
-            (let ([rel* (hash-set rel var #t)])
-              (cons ts (slice-trace* trace-rest rel*)))]
-           [(relevant? rel dist)
-            (cons ts (slice-trace* trace-rest rel))]
-           [else (slice-trace* trace-rest rel)])]
-    ['() '()]))
-
-;; relevant? : Hash[TraceVar => #t] TraceExpr -> Boolean
-(define (relevant? rel te)
-  (match te
-    [(t:quote _) #f]
-    [(t:cons e1 e2) (or (relevant? rel e1) (relevant? rel e2))]
-    [(t:dist _ args) (any-relevant? rel args)]
-    [(? symbol? tvar) (hash-ref rel tvar #f)]))
-
-;; any-relevant? : Hash[TraceVar => #t] (Listof TraceExpr) -> Boolean
-(define (any-relevant? rel tes)
-  (for/or ([te tes]) (relevant? rel te)))
-
-;; TODO:
-;; - do HMC via slice
-;; - do slice-sampling via slice
-
 ;; ------------------------------------------------------------
 ;; Slicing for Gibbs sampling
 
---
+++ trace-slice.rkt
@@ -0,0 +1,43 @@
+#lang racket/base
+(require (rename-in racket/match [match-define defmatch])
+         gamble
+         "base.rkt"
+         "lightweight-mh.rkt"
+         "trace-mh.rkt")
+(provide (all-defined-out))
+
+;; TODO:
+;; - do slice-sampling via slice
+
+;; slice-trace : Trace TraceVar -> Trace
+(define (slice-trace trace tvar)
+  (slice-trace* trace (hash tvar #t)))
+
+;; slice-trace* : Trace Hash[Tvar => #t] -> Trace
+(define (slice-trace* trace rel)
+  (match trace
+    [(cons (and (ts:primop var primop args) ts) trace-rest)
+     (cond [(any-relevant? rel args)
+            (let ([rel* (hash-set rel var #t)])
+              (cons ts (slice-trace* trace-rest rel*)))]
+           [else (slice-trace* trace-rest rel)])]
+    [(cons (and (ts:sample var dist value) ts) trace-rest)
+     (cond [(relevant? rel value)
+            (let ([rel* (hash-set rel var #t)])
+              (cons ts (slice-trace* trace-rest rel*)))]
+           [(relevant? rel dist)
+            (cons ts (slice-trace* trace-rest rel))]
+           [else (slice-trace* trace-rest rel)])]
+    ['() '()]))
+
+;; relevant? : Hash[TraceVar => #t] TraceExpr -> Boolean
+(define (relevant? rel te)
+  (match te
+    [(t:quote _) #f]
+    [(t:cons e1 e2) (or (relevant? rel e1) (relevant? rel e2))]
+    [(t:dist _ args) (any-relevant? rel args)]
+    [(? symbol? tvar) (hash-ref rel tvar #f)]))
+
+;; any-relevant? : Hash[TraceVar => #t] (Listof TraceExpr) -> Boolean
+(define (any-relevant? rel tes)
+  (for/or ([te tes]) (relevant? rel te)))
